ce42ad35121ee44464bc8255b0fdf82b43c70f0a
Bojan Dimic <bdimic@bluecatnetworks.com>
2024-12-08 15:44:47 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+
+from constants import ASTEROID_KINDS
+from constants import ASTEROID_MAX_RADIUS
+from constants import ASTEROID_MIN_RADIUS
+from constants import ASTEROID_SPAWN_RATE
+from constants import SCREEN_HEIGHT
+from constants import SCREEN_WIDTH
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        pygame.display.flip()
+    
+if __name__ == "__main__":
+    main()
