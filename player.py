95d7f44903f38b41a11082857f16f6a73204c4a8
Bojan Dimic <bdimic@bluecatnetworks.com>
2024-12-08 17:30:53 +0100

    Limit player shoot time to every 0.3 seconds.

--- player.py
+++ player.py
@@ -4,13 +4,14 @@ from circleshape import CircleShape
 from shoot import Shot
 from constants import PLAYER_RADIUS
 from constants import PLAYER_SPEED
-from constants import PLAYER_SHOOT_SPEEED
+from constants import PLAYER_SHOOT_COOLDOWN
 from constants import PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
         # in the player class
     def triangle(self):
@@ -35,9 +36,12 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             self.rotate(dt)
         if keys[pygame.K_SPACE]:
-            self.shot(dt)
-
-    def shot(self, dt):
+            if not self.timer > 0: 
+                self.shot()
+        self.timer -= dt
+        
+    def shot(self):
+        self.timer = PLAYER_SHOOT_COOLDOWN
         new_shot = Shot(self.position.x, self.position.y)
         new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
 
