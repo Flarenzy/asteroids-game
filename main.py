f1c114069c3383fa2476747b8806bfe960cac9d7
Bojan Dimic <bdimic@bluecatnetworks.com>
2024-12-08 15:48:27 +0100

    Set frame rate to 60.

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt: float = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -20,6 +22,7 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
